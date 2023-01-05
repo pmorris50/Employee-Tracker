1d694a32a2713eb3e0e5e5d6bcd67786e072bdab
pmorris50 <patrickmorris532@gmail.com>
2023-01-04 19:28:39 -0700

    quicksave

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 //grabbing packages
-const express = require('express');
 const inquirer = require('inquirer');
+const express = require('express');
 const mysql = require('mysql2');
 const path = require('path')
 const cTable =require('console.table');
 const PORT = process.env.PORT || 3001
-
-const app = express;
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
 const db = mysql.createConnection({
-    host: POST,
+    host: PORT,
     user: 'root',
     password: 'rootroot',
     database: 'company_db'
